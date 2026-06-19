bb38be0dd5493b72c9fe291974c824e3cf651de4
srirama7 <amoghabhat7403@gmail.com>
2026-06-19 16:39:24 +0530

    fix: remove WebView wv flag from user agent to fix Google OAuth disallowed_useragent error

--- android/app/src/main/java/com/propnest/app/MainActivity.java
+++ android/app/src/main/java/com/propnest/app/MainActivity.java
@@ -1,5 +1,23 @@
 package com.bhoomitayi.app;
 
+import android.os.Bundle;
+import android.webkit.WebView;
 import com.getcapacitor.BridgeActivity;
 
-public class MainActivity extends BridgeActivity {}
+public class MainActivity extends BridgeActivity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        // Fix: Google OAuth blocks sign-in in Android WebView by detecting
+        // the "; wv)" substring in the user agent (disallowed_useragent error).
+        // We remove it so the WebView is treated as a regular Chrome browser.
+        WebView webView = getBridge().getWebView();
+        String originalUserAgent = webView.getSettings().getUserAgentString();
+        String fixedUserAgent = originalUserAgent
+            .replace("; wv)", ")")
+            .replace("; wv", "");
+        webView.getSettings().setUserAgentString(fixedUserAgent);
+    }
+}
